dc2b9c00734f4dc8c989e0fc89f2dc777b2437d4
esandoval <xsandoval21x@gmail.com>
2020-04-24 20:45:13 -0700

    Add game functionality with javascript. Add functions to gameBoard module and displayController module

--- script.js
+++ script.js
@@ -1,8 +1,67 @@
+let playerTurn;
+
 const gameBoard = (() => {
-    let grid = [['', '', ''],
-                ['', '', ''],
-                ['', '', '']];
+    const cellElements = document.querySelectorAll("[data-cell]");
+    const WIN_COMBOS = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
     
+    const handleClick = (e) => {
+        const square = e.target;
+        playerTurn = playerTurn === "X"? "X" : "O";
+        placeMark(square, playerTurn);
+        if(checkWin(playerTurn)) {
+            displayController.endGame(false);
+        } else if(checkDraw()) {
+            displayController.endGame(true);
+        } else {
+            changeTurn();
+        }
+    }
+
+    const setGameBoard = () => {
+        cellElements.forEach(cell => {
+            cell.innerText = "";
+            cell.removeEventListener("click", handleClick);
+            cell.addEventListener("click", handleClick, {once: true});
+        });
+    }
+
+    setGameBoard();
+    
+    const placeMark = (square, marker) => {
+        square.innerText = marker;
+    }
+    const changeTurn = () => {
+        playerTurn = playerTurn === "X"? "O" : "X";
+    }
+
+    const checkWin = (currentTurn) => {
+        return WIN_COMBOS.some(combination => {
+            return combination.every(index => {
+                return cellElements[index].textContent.includes(currentTurn);
+            })
+        });
+    }
+
+    const checkDraw = () => {
+        return [...cellElements].every(cell => {
+            return cell.textContent.includes("X") || cell.textContent.includes("O");
+        })
+    }
+
+    return {
+        cellElements,
+        setGameBoard,
+    }
 })();
 
 const Player = (name, marker) => {
@@ -10,5 +69,28 @@ const Player = (name, marker) => {
 }
 
 const displayController = (() => {
+    const winningMessageTextElement = document.querySelector("[data-winning-message-text]");
+    const winningMessageElement = document.getElementById("winningMessage");
+    const restartButton = document.getElementById("restart-button");
 
+    const endGame = (draw) => {
+        if(draw){
+            winningMessageTextElement.innerText = "Draw...Try again!";
+        }else {
+            winningMessageTextElement.innerText = `${playerTurn} Wins!`;
+        }
+        winningMessageElement.classList.add("show");
+    }
+
+    const restartGame = () => {
+        gameBoard.setGameBoard();
+        winningMessageTextElement.innerText = "";
+        winningMessageElement.classList.remove("show");
+    }
+
+    restartButton.addEventListener("click", restartGame);
+
+    return {
+        endGame,
+    }
 })();
