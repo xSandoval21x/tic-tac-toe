c9489d5a0ec58c3aa0a1ffefc4be3963aa2e085d
esandoval <xsandoval21x@gmail.com>
2020-04-27 15:55:08 -0700

    Add start/restart/reset buttons and input for user to input name for each player. Add CSS styling and colors

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let playerTurn;
+let playerTurn = "X";
 
 const gameBoard = (() => {
+    const board = document.getElementById("board");
     const cellElements = document.querySelectorAll("[data-cell]");
     const WIN_COMBOS = [
         [0, 1, 2],
@@ -33,15 +34,18 @@ const gameBoard = (() => {
             cell.removeEventListener("click", handleClick);
             cell.addEventListener("click", handleClick, {once: true});
         });
+        playerTurn = "X";
     }
 
     setGameBoard();
     
     const placeMark = (square, marker) => {
         square.innerText = marker;
+        square.style.color = marker === "X"? "rgb(255, 108, 85)" : "rgb(107, 107, 255)";
     }
     const changeTurn = () => {
         playerTurn = playerTurn === "X"? "O" : "X";
+        displayController.setMessage();
     }
 
     const checkWin = (currentTurn) => {
@@ -55,42 +59,100 @@ const gameBoard = (() => {
     const checkDraw = () => {
         return [...cellElements].every(cell => {
             return cell.textContent.includes("X") || cell.textContent.includes("O");
-        })
+        });
     }
 
     return {
+        board,
         cellElements,
         setGameBoard,
     }
 })();
 
 const Player = (name, marker) => {
-    
+    const getName = () => {
+        return name;
+    }
+
+    return {
+        getName,
+    }
 }
 
 const displayController = (() => {
+    const startButton = document.getElementById("start-button");
     const winningMessageTextElement = document.querySelector("[data-winning-message-text]");
     const winningMessageElement = document.getElementById("winningMessage");
+    const resetButton = document.getElementById("reset-button");
     const restartButton = document.getElementById("restart-button");
+    const message = document.querySelector(".message");
+    let playerX;
+    let playerO;
+
+    const showGrid = () => {
+        createPlayers();
+        message.classList.add("show");
+        gameBoard.board.classList.add("show");
+        startButton.parentElement.classList.remove("show");
+        resetButton.parentElement.classList.add("show");
+        restartGame();   
+    }
+
+    const createPlayers = () => {
+        const playerXInput = document.getElementById("x-player");
+        const playerOInput = document.getElementById("o-player");
+        playerX = Player(playerXInput.value, "X");
+        playerO = Player(playerOInput.value, "O");
+        playerXInput.style.border = "none";
+        playerOInput.style.border = "none";
+        setMessage();
+    }
+
+    const setMessage = () => {
+        if(playerTurn === "X") {
+            message.textContent = `${playerX.getName()} is up!`;
+        }else if(playerTurn === "O") {
+            message.textContent = `${playerO.getName()}'s turn`;
+        } else {
+            message.textContent = "";
+        }
+    }
 
     const endGame = (draw) => {
         if(draw){
             winningMessageTextElement.innerText = "Draw...Try again!";
         }else {
-            winningMessageTextElement.innerText = `${playerTurn} Wins!`;
+            winningMessageTextElement.innerText = `${playerTurn === "X"? playerX.getName() : playerO.getName()} Wins!`;
         }
+        resetButton.parentElement.classList.remove("show");
         winningMessageElement.classList.add("show");
     }
 
     const restartGame = () => {
         gameBoard.setGameBoard();
+        setMessage();
         winningMessageTextElement.innerText = "";
         winningMessageElement.classList.remove("show");
     }
 
-    restartButton.addEventListener("click", restartGame);
+    const reset = () => {
+        const playerXInput = document.getElementById("x-player");
+        const playerOInput = document.getElementById("o-player");
+        playerXInput.style.border = "1px solid black";
+        playerOInput.style.border = "1px solid black";
+        message.classList.remove("show");
+        gameBoard.board.classList.remove("show");
+        resetButton.parentElement.classList.remove("show");
+        startButton.parentElement.classList.add("show");
+        restartGame();
+    }
+
+    startButton.addEventListener("click", showGrid);
+    resetButton.addEventListener("click", reset);
+    restartButton.addEventListener("click", showGrid);
 
     return {
         endGame,
+        setMessage,
     }
 })();
